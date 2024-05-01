ff294ca78fb4fddda0f14b1f3c6c31867538cc09
douglascugliaridev <douglas.cugliari@gmail.com>
2024-04-30 22:22:23 -0300

    Atualizar estilização e inclusão leitura arquivo JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
+import { ProdutosContextProvider } from "../src/context/ProdutosContext";
 import "./App.css";
 
 /* eslint-disable */
 function App() {
  return (
   <div className="root">
-   <Header />
-   <Outlet />
-   <Footer />
+   <ProdutosContextProvider>
+    <Header />
+    <Outlet />
+    <Footer />
+   </ProdutosContextProvider>
   </div>
  );
 }
